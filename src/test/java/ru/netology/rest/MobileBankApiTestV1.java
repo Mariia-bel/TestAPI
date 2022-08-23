9a5af61ac72277cd9c82e514934d7122f2376f89
Мария <airbelka@bk.ru>
2022-08-23 21:06:09 +0300

    Update test1

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -3,6 +3,7 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 class MobileBankApiTestV1 {
     @Test
@@ -13,9 +14,11 @@ class MobileBankApiTestV1 {
                 .baseUri("http://localhost:9999/api/v1")
                 // Выполняемые действия
                 .when()
-                .get("/demo/accounts")
+                    .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(200);
+                    .statusCode(200)
+                    .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
     }
 }
